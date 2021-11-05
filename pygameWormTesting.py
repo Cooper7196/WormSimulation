0cb8a733a8c76273666c400e890db4bd5d90da98
Cooper7196 <cbrosgall@gmail.com>
2021-11-04 20:19:39 -0700

    Made work drawing framework

---
+++ pygameWormTesting.py
@@ -0,0 +1,84 @@
+import pygame
+import math
+
+class Worm:
+    def __init__(self, screen, length, x, y):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        
+        self.head = WormNode(screen, x, y, 20)
+        self.body = [self.head]
+
+        for i in range(length):
+            tmpWorm = WormNode(
+                screen, self.x, self.y, 20, leader=len(
+                    self.body) - 1)
+            tmpWorm.oldX = tmpWorm.x
+            tmpWorm.oldY = tmpWorm.y
+
+            self.body.append(tmpWorm)
+
+    def update(self):
+
+        self.head.oldX = self.x
+        self.head.oldY = self.y
+
+        self.head.x = self.x
+        self.head.y = self.y
+
+        for node in self.body[1:]:
+            node.oldX = node.x
+            node.oldY = node.y
+
+            node.x = self.body[node.leader].oldX
+            node.y = self.body[node.leader].oldY
+
+    def draw(self):
+        for node in self.body:
+            node.draw()
+
+class WormNode:
+    def __init__(self, screen, x, y, radius, leader=None):
+        self.x, self.y = x, y
+        self.oldX, self.oldY = x, y
+        self.radius = radius
+        self.screen = screen
+        self.leader = leader
+
+    def draw(self):
+        """ Draw the worm """
+        pygame.draw.circle(self.screen, (255, 255, 255), [self.x, self.y], self.radius, 0)
+
+
+# Window dimensions.
+width = 800
+height = 800
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+worm = Worm(screen, 100, width/2, height/2)
+clock = pygame.time.Clock()
+running = True
+
+speed = 3
+rotation = 0
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        rotation -= 3
+    elif keys[pygame.K_d]:
+        rotation += 3
+
+    worm.x += math.sin(math.radians(rotation)) * speed
+    worm.y -= math.cos(math.radians(rotation)) * speed
+
+    screen.fill((0, 0, 0))
+    worm.update()
+    worm.draw()
+    pygame.display.update()
+    clock.tick(60)
